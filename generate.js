f7f50a4dc5ac66e852a4df050e0659c95722efaf
cottonatocd <cotton@ocdagency.com>
2021-03-02 14:30:27 -0500

    generated files

--- generate.js
+++ generate.js
@@ -8,20 +8,24 @@ glob("_data/*.yaml", function (er, files) {
    const contents = files.map((f) => {
       const id = f.split('-')[0].split('/')[1];
       const data = YAMLJS.load(fs.readFileSync(f).toString());
-      
+
+      if(!fs.existsSync(`_lots/${id}`)){
      
-      data['lots'].forEach(lot => {
-         fs.existsSync(`_lots`) || fs.mkdirSync(`_lots`);
-         fs.existsSync(`_lots/${id}`) || fs.mkdirSync(`_lots/${id}`);
-         fs.writeFile(`_lots/${id}/${lot["LOT"]}.md`, `---\nSALE: "${id}"\n`+YAML.stringify(lot)+'\n---', (err) => {
-            if (err)
-               console.log(err);
-            else {
-               console.log(`${lot["LOT"]} -- File written successfully`);
-            }
+         data['lots'].forEach(lot => {
+            fs.existsSync(`_lots`) || fs.mkdirSync(`_lots`);
+            fs.existsSync(`_lots/${id}`) || fs.mkdirSync(`_lots/${id}`);
+            fs.writeFile(`_lots/${id}/${lot["LOT"]}.md`, `---\nSALE: "${id}"\n`+YAML.stringify(lot)+'\n---', (err) => {
+               if (err)
+                  console.log(err);
+               else {
+                  console.log(`${lot["LOT"]} -- File written successfully`);
+               }
+            });
          });
-      });
+
+      }
 
    });
 
 });
+
